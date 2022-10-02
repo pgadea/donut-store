732f566a19f0b10969a7f77f96313c1fc3c7c4ed
Pierre Gadea <pgadea7@gmail.com>
2022-10-02 15:43:53 -0500

    first commit

---
+++ src/app/admin/services/donut.service.ts
@@ -0,0 +1,103 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+
+import { catchError, delay, map, of, retryWhen, take, tap, throwError } from 'rxjs';
+
+import { Donut } from '../models/donut.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DonutService {
+  private donuts: Donut[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  read() {
+    if (this.donuts.length) {
+      return of(this.donuts);
+    }
+
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    headers = headers.append('Api-Token', '123456');
+
+    const options = {
+      headers,
+    };
+
+    return this.http.get<Donut[]>(`/api/donuts`, options).pipe(
+      tap((donuts) => {
+        this.donuts = donuts;
+      }),
+      retryWhen((errors) => errors.pipe(delay(1000), take(2))),
+      catchError(this.handleError)
+    );
+  }
+
+  readOne(id: string | null) {
+    return this.read().pipe(
+      map((donuts) => {
+        const donut = donuts.find((donut: Donut) => donut.id === id);
+
+        if (donut) {
+          return donut;
+        }
+
+        return {
+          name: '',
+          icon: '',
+          price: 0,
+          description: '',
+        };
+      })
+    );
+  }
+
+  create(payload: Donut) {
+    return this.http.post<Donut>(`/api/donuts`, payload).pipe(
+      tap((donut) => {
+        this.donuts = [...this.donuts, donut];
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  update(payload: Donut) {
+    return this.http.put<Donut>(`/api/donuts/${payload.id}`, payload).pipe(
+      tap((donut) => {
+        this.donuts = this.donuts.map((item: Donut) => {
+          if (item.id === payload.id) {
+            return donut;
+          }
+          return item;
+        });
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  delete(payload: Donut) {
+    return this.http.delete<Donut>(`/api/donuts/${payload.id}`).pipe(
+      tap(() => {
+        this.donuts = this.donuts.filter(
+          (donut: Donut) => donut.id !== payload.id
+        );
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    if (err.error instanceof ErrorEvent) {
+      // client-side
+      console.warn('Client', err.message);
+    } else {
+      // server-side
+      console.warn('Server', err.status);
+    }
+    return throwError(() => new Error(err.message));
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AdminModule } from './admin/admin.module';
+
+export const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'admin',
+  },
+  {
+    path: '**',
+    redirectTo: 'admin',
+  },
+];
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes), AdminModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
